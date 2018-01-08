0cb117873a0a9b5d7b787cd66eec7ee9c5261e8f
Luke Kreft <lkreft@liquidweb.com>
2018-01-07 23:45:52 -0500

    moved

--- grimm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Author: Luke Kreft
-
-
-crit() { echo "Error: $*"; exit;}
-[ -e config.sh ] && source config.sh || crit "could not find config file"
-(( $( id -u ) )) && crit "must be root"
-[ -e /etc/debian_version ] || [ -e /etc/redhat-release ] || crit "script only supports cent and debian at this time"
